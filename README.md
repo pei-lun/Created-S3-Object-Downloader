510752a168410b87887027d6c3be7ee332e036cd
Pei-Lun Huang <peilun0912@gmail.com>
2019-09-10 16:28:57 +0800

    feat: initial version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Created S3 Object Downloader
 
+This CLI tool polls SQS queue for new object created events published by S3, and then it downloads objects to specified path.
---
+++ download.py
@@ -0,0 +1,55 @@
+import json
+from os import makedirs
+from os.path import dirname, isdir, join
+
+import boto3
+import click
+
+
+def receive_sqs_msgs(sqs_queue_url: str):
+    sqs = boto3.resource('sqs')
+    sqs_queue = sqs.Queue(sqs_queue_url)
+
+    msgs = sqs_queue.receive_messages()
+    while msgs:
+        for msg in msgs:
+            yield msg
+        msgs = sqs_queue.receive_messages()
+
+
+@click.command()
+@click.option('--reserved-prefixes', default=0, type=int)
+@click.argument('sqs_queue_url')
+@click.argument(
+    'destination',
+    type=click.Path(
+        exists=True, file_okay=False, writable=True, resolve_path=True
+    ),
+)
+def download(sqs_queue_url, destination, reserved_prefixes):
+    s3_client = boto3.client('s3')
+
+    for msg in receive_sqs_msgs(sqs_queue_url):
+        event: dict = json.loads(msg.body)['Records'][0]
+        event_src: str = event['eventSource']
+        event_name: str = event['eventName']
+
+        if event_src == 'aws:s3' and event_name.startswith('ObjectCreated'):
+            bucket_name: str = event['s3']['bucket']['name']
+            object_key: str = event['s3']['object']['key']
+
+            filename = join(
+                destination, *object_key.split('/')[-1 - reserved_prefixes :]
+            )
+
+            directory = dirname(filename)
+            if not isdir(directory):
+                makedirs(directory)
+
+            s3_client.download_file(bucket_name, object_key, filename)
+
+            msg.delete()
+
+
+if __name__ == '__main__':
+    download()
